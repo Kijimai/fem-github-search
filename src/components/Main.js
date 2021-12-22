1bee4e3695dd92640a6a16dec868a7cdde7c9faa
Jibbyworks <JDBucog@yahoo.com>
2021-12-22 15:47:11 -0800

    mobile style searchbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Navbar from "./Navbar"
+import Searchbar from "./Searchbar"
+
 const Main = () => {
   return (
     <main>
       <Navbar />
+      <Searchbar />
     </main>
   )
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React from "react"
+import search from "../assets/icon-search.svg"
 const Searchbar = () => {
   return (
     <div className="searchbar">
-      <form action="searchbar__form">
-        <input type="text" />
+      <form onSubmit={(e) => e.preventDefault()} className="searchbar__form">
+        <img className="icon" src={search} alt="search magnifying glass" />
+        <input
+          className="search"
+          type="email"
+          placeholder="Search Github username"
+        />
+        <button className="btn blue">Search</button>
       </form>
     </div>
   )
